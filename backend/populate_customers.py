770d457cd5cadfe0d7f1617806cc07ad4d491350
Vincent Mugendi <125495305+vincent-mugendi@users.noreply.github.com>
2024-04-04 18:06:53 +0300

    Update populate_customers.py

--- backend/populate_customers.py
+++ backend/populate_customers.py
@@ -4,7 +4,6 @@ import os
 import sys
 from flask_sqlalchemy import SQLAlchemy
 
-# Add the path to the parent directory of the 'backend' directory to the Python path
 current_dir = os.path.dirname(os.path.realpath(__file__))
 parent_dir = os.path.dirname(current_dir)
 sys.path.append(parent_dir)
@@ -15,12 +14,10 @@ from app import app, db, Customer
 app.app_context().push()
 
 def populate_customers():
-    # Sample data for 10 customers
+    # Sample data for 2 customers
     sample_customers = [
         {'customer_id': 1, 'name': 'John Doe', 'email': 'john.doe@example.com', 'phone_number': '1234567890', 'address': '123 Main St'},
-        {'customer_id': 2, 'name': 'Jane Smith', 'email': 'jane.smith@example.com', 'phone_number': '0987654321', 'address': '456 Elm St'},
-        # Add more sample customer data here
-        # Make sure to use unique customer_id values
+        {'customer_id': 2, 'name': 'Jane Smith', 'email': 'jane.smith@example.com', 'phone_number': '0987654321', 'address': '456 Elm St'
     ]
 
     # Populate the customers table with sample data
